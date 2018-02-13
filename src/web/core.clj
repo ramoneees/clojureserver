7af01c30717c0a120d7296f3a03443b26bc3817c
ramonprios <ramonp.rios@gmail.com>
2018-02-13 12:32:23 -0300

    uping a server made using clojure

---
+++ src/web/core.clj
@@ -0,0 +1,19 @@
+(ns web.core
+  (:require [org.httpkit.server :as s]
+    [compojure.core :refer [routes POST GET ANY]]))
+
+(defn app []
+  (routes
+    (GET "/" [:as req]
+      {:status 200
+      :headers{"Content-type" "text/html"}
+      :body "<h1> Surprise motherfucker </h1>" })))
+
+
+  (defn create-server []
+    (s/run-server (app) {:port 8080}))
+
+  (defn stop-server [server]  
+   (server :timeout 200))
+    
+
